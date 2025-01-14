195e1ee0b65e189380cac9ba66f38900db906518
yohann-dev <yohann_88@hotmail.fr>
2025-01-14 22:35:06 +0200

    added reply function

--- backend/main.js
+++ backend/main.js
@@ -9,60 +9,60 @@ let character = loadCharacterFromJson("character.yaml")
 let newsRssUrl = character.newsRssUrl
 let moods = character.available_moods
 const actions = {
-    // postATweet: {
-    //     probability: character.actionProbabilities.postATweet,
-    //     callback: () => {
-    //        let mood = moods[Math.floor(Math.random() * moods.length)];
-    //        console.log(`I am ${mood}... let's make some post`);
-    //        makeATextPost(character, mood)
-    //     }
-    // },
-    // postATrendTweet: {
-    //     probability: character.actionProbabilities.postATrendTweet,
-    //     callback: () => {
-    //        let mood = moods[Math.floor(Math.random() * moods.length)];
-    //        console.log(`I am ${mood}... let's make some post`);
-    //        makeATrendPost(character, mood)
-    //     }
-    // },
-    // readSomeNews: {
-    //     probability: character.actionProbabilities.readSomeNews,
-    //     callback: () => {
-    //         readRandomNews(newsRssUrl)
+    postATweet: {
+        probability: character.actionProbabilities.postATweet,
+        callback: () => {
+           let mood = moods[Math.floor(Math.random() * moods.length)];
+           console.log(`I am ${mood}... let's make some post`);
+           makeATextPost(character, mood)
+        }
+    },
+    postATrendTweet: {
+        probability: character.actionProbabilities.postATrendTweet,
+        callback: () => {
+           let mood = moods[Math.floor(Math.random() * moods.length)];
+           console.log(`I am ${mood}... let's make some post`);
+           makeATrendPost(character, mood)
+        }
+    },
+    readSomeNews: {
+        probability: character.actionProbabilities.readSomeNews,
+        callback: () => {
+            readRandomNews(newsRssUrl)
            
-    //     }
-    // },
-    // doNothing: {
-    //     probability: character.actionProbabilities.doNothing,
-    //     callback: () => {
-    //         let action = "Doing nothing, just chillin'!" 
-    //         console.log(action)
-    //         addMemory(action)
-    //     }
-    // },
-    // sleeping: {
-    //     probability: character.actionProbabilities.sleeping,
-    //     callback: () => {
-    //         console.log("let's get some sleep")
-    //         sleep()
-    //     }
-    // },
-    // postAPicture: {
-    //     probability: character.actionProbabilities.postAPicture,
-    //     callback: () => {
-    //         let mood = moods[Math.floor(Math.random() * moods.length)];
-    //         console.log(`I am ${mood}... let's make some art`);
-    //         makeAPicturePost(character, mood)
-    //     }
-    // },
-    // postATrendPicture: {
-    //     probability: character.actionProbabilities.postATrendPicture,
-    //     callback: () => {
-    //         let mood = moods[Math.floor(Math.random() * moods.length)];
-    //         console.log(`I am ${mood}... let's make some art`);
-    //         makeATrendPicturePost(character, mood)
-    //     }
-    // },
+        }
+    },
+    doNothing: {
+        probability: character.actionProbabilities.doNothing,
+        callback: () => {
+            let action = "Doing nothing, just chillin'!" 
+            console.log(action)
+            addMemory(action)
+        }
+    },
+    sleeping: {
+        probability: character.actionProbabilities.sleeping,
+        callback: () => {
+            console.log("let's get some sleep")
+            sleep()
+        }
+    },
+    postAPicture: {
+        probability: character.actionProbabilities.postAPicture,
+        callback: () => {
+            let mood = moods[Math.floor(Math.random() * moods.length)];
+            console.log(`I am ${mood}... let's make some art`);
+            makeAPicturePost(character, mood)
+        }
+    },
+    postATrendPicture: {
+        probability: character.actionProbabilities.postATrendPicture,
+        callback: () => {
+            let mood = moods[Math.floor(Math.random() * moods.length)];
+            console.log(`I am ${mood}... let's make some art`);
+            makeATrendPicturePost(character, mood)
+        }
+    },
     replyToMentions: {
         probability: character.actionProbabilities.replyToMentions,
         callback: () => {
